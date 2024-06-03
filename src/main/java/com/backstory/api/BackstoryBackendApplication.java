f023a4d5eea421d123b7dc3d0efc7a261fbfa102
Aiden Harrington <aidenharrington@Aidens-MacBook-Pro.local>
2024-06-03 11:30:18 -0700

    first commit

---
+++ src/main/java/com/backstory/api/BackstoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backstory.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstoryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backstory/api/BackstoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backstory.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
